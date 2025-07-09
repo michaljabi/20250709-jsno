b4d71077438eca71cb8e06fa71c46cde74314dae
Jabi <michal.jabi@gmail.com>
2025-07-09 11:46:17 +0200

    task(3)[step-3]: implement logic, use readline

--- JSNO-zadania/3-read-user-input/banner-generator/src/main.js
+++ JSNO-zadania/3-read-user-input/banner-generator/src/main.js
@@ -1,3 +1,29 @@
-setInterval(() => {
-    console.log('🤣?');
-}, 2000);
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+console.log(`
+****************************************
+*   WITAJ W GENERATORZE BANNERÓW [!]   *
+****************************************    
+`)
+// const test = "to jest moje zdanie";
+// makeBanner(test);
+
+function makeBanner(sentence) {
+    const words = sentence.toUpperCase().split(' ');
+    for(const word of words) {
+        console.log('*'.repeat(word.length + 4))
+        console.log('*', word, '*');
+        console.log('*'.repeat(word.length + 4))
+    }
+    return words;
+}
+
+const rl = readline.createInterface({ input, output });
+
+rl.question('Podaj zdanie jakie chcesz zamienić w banner:', (answer) => {
+
+  makeBanner(answer);
+  rl.close();
+  
+});
