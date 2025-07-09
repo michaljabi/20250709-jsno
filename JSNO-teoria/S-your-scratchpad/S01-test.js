a04489a2f935f74bf6bdc33059d1212afdb54117
Jabi <michal.jabi@gmail.com>
2025-07-09 14:49:44 +0200

    explain: how to use JS REPL extension.

---
+++ JSNO-teoria/S-your-scratchpad/S01-test.js
@@ -0,0 +1,34 @@
+
+
+const x = 112;
+
+console.log(x + 200); //=
+
+x + 900 * 2; //=
+
+function sayHello() {
+    console.log('hello')
+}
+
+sayHello()
+sayHello()
+
+const user = {name: 'Michał', lastName: 'Kowalsky'}
+
+console.log(user)
+
+try {
+    console.log(xxx);
+    console.log('!')
+    console.log('!')
+    console.log('!')
+    console.log('!')
+    console.log('!')
+    console.log('!')
+    console.log('!')
+} catch(e) {
+    console.log(e.message);
+}
+
+sayHello()
+sayHello()
