40dfecb2796fa26aac59550427abeb70280fa8b6
Jabi <michal.jabi@gmail.com>
2025-07-09 12:02:49 +0200

    refactor(3)[step-4]: make it right.

--- JSNO-zadania/3-read-user-input/banner-generator/src/main.js
+++ JSNO-zadania/3-read-user-input/banner-generator/src/main.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 
@@ -6,24 +8,30 @@ console.log(`
 *   WITAJ W GENERATORZE BANNERÓW [!]   *
 ****************************************    
 `)
-// const test = "to jest moje zdanie";
-// makeBanner(test);
-
-function makeBanner(sentence) {
-    const words = sentence.toUpperCase().split(' ');
-    for(const word of words) {
-        console.log('*'.repeat(word.length + 4))
-        console.log('*', word, '*');
-        console.log('*'.repeat(word.length + 4))
-    }
-    return words;
-}
 
 const rl = readline.createInterface({ input, output });
 
-rl.question('Podaj zdanie jakie chcesz zamienić w banner:', (answer) => {
+
+rl.question('Podaj zdanie jakie chcesz zamienić w banner:', (answer = '') => {
 
   makeBanner(answer);
   rl.close();
-  
+
 });
+
+// TODO: (zad-4) przenieś do innego pliku
+
+const PADDING = 4;
+
+/**
+ * @param {string} sentence 
+ */
+function makeBanner(sentence) {
+    const words = sentence.toUpperCase().split(' ');
+    for(const word of words) {
+        const line = '*'.repeat(word.length + PADDING);
+        console.log(`${line}
+* ${word} *
+${line}`)
+    }
+}
