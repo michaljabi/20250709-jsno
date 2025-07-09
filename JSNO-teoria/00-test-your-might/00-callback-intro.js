c1e0ccac70b90fe25966ddf0ed7635139c61661d
Jabi <michal.jabi@gmail.com>
2025-07-09 15:04:53 +0200

    task(00 teoria): get value using callback

--- JSNO-teoria/00-test-your-might/00-callback-intro.js
+++ JSNO-teoria/00-test-your-might/00-callback-intro.js
@@ -1,8 +1,14 @@
 
 console.log('Hello stranger... shall we start?')
 
+
+// PROVIDER:
 function giveMeTheNumber(mistery) {
-    mistery(300)
+    mistery(100)
+    mistery(100)
+    setTimeout(() => {
+         mistery(100)
+    }, 3000)
 }
 
 /**
@@ -11,3 +17,44 @@ function giveMeTheNumber(mistery) {
  * Poniżej odbierz liczbę 300 i pokaż ją na ekranie:
  * */
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+setTimeout(() => {
+    // CONSUMER #1:
+    giveMeTheNumber((value) => {
+        console.log(value);
+    })
+}, 2000)
+
+
+
+
+
+
+// CONSUMER #2:
+giveMeTheNumber((value) => {
+    console.log(value);
+})
+
+
+setTimeout(() => {
+         console.log(1002)
+    }, 5000)
