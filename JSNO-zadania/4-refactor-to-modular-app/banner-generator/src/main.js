ab4229a7c17fb7a20ecec6787d14afce00245131
Jabi <michal.jabi@gmail.com>
2025-07-09 14:19:40 +0200

    task(4)[step-5]: full program logic.

--- JSNO-zadania/4-refactor-to-modular-app/banner-generator/src/main.js
+++ JSNO-zadania/4-refactor-to-modular-app/banner-generator/src/main.js
@@ -11,10 +11,29 @@ const rl = createInterface({
   output,
 });
 
-rl.question("Podaj zdanie jakie chcesz zamienić w banner:", (answer = "") => {
-  makeBanner(answer);
-  rl.close();
-});
 
+function app() {
+  rl.question("Podaj zdanie jakie chcesz zamienić w banner:", (answer = "") => {
+    makeBanner(answer);
+    askWhatNext();
+  });
+}
 
+function askWhatNext() {
+  rl.question(`Co robimy dalej - wpisz odpowiednią literę:
+  [k]oniec
+  [n]astępny banner
+  _ `, (letter) => {
+    if(letter === 'k') {
+      console.log('Kończę działanie, żegnam...')
+      rl.close();
+    } else if(letter === 'n') {
+      app();
+    } else {
+      console.log(`Nieobsługiwany wybór "${letter}"!`)
+      askWhatNext();
+    }
+  })
+}
 
+app();
