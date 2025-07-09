e91017d45e832c8698071d55cfb72b316694b935
Jabi <michal.jabi@gmail.com>
2025-07-09 15:21:07 +0200

    task(01 teoria): throw and catch error

--- JSNO-teoria/00-test-your-might/01-code-with-error-handling.js
+++ JSNO-teoria/00-test-your-might/01-code-with-error-handling.js
@@ -4,3 +4,21 @@
  * Zrób funkcję, która dzieli 2 liczby
  * i rzuca błąd "Cannot divide by 0" jeśli 2 liczba to "0"
  * */
+
+function divide(a = 0, b = 1) {
+ 
+    if(b === 0) {
+        throw new Error("Cannot divide by 0")
+    }
+    return a / b;
+}
+
+try {
+    divide(10, 1) //=
+    divide(0, 1) //=
+    divide(10, 5) //=
+    divide(10, 0) //=
+    divide(10, 0) //=
+} catch (e) {
+    console.log(e.message)
+}
