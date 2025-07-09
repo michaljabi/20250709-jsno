51a2f09daf4e71d7721ffeaae2374ad81f920ce3
Jabi <michal.jabi@gmail.com>
2025-07-09 15:29:01 +0200

    task(02 teoria): make it work

--- JSNO-teoria/00-test-your-might/02-more-complex-logic.js
+++ JSNO-teoria/00-test-your-might/02-more-complex-logic.js
@@ -10,13 +10,39 @@
  * */
 
 
+function divideAync(a = 0, b = 1, collect = () => {}) {
+ 
+    if(b === 0) {
+        throw new Error("Cannot divide by 0")
+    }
+
+    setTimeout(() => {
+        collect(a / b)
+    }, 3000)
+}
 
 
 
+divideAync(10, 2, (result) => {
+    console.log(result)
+})
 
 /**
  * 2.2
  *
  * Jak teraz możemy obsłużyć błąd "Cannot divide by 0"?
  * - zaproponuj rozwiązanie
+ * 
+ * 
+ * bo hmmmm try catch nie zadziała ?
  * */
+
+// innymi słowy "jak poinformować Consumera, że coś poszło nie tak."
+
+try {
+    divideAync(10, 0, (result) => {
+        console.log(result)
+    })
+} catch (e) {
+    console.log(e.message)
+}
