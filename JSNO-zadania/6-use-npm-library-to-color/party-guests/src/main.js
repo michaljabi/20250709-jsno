280f5773fedf1acb578abcf1681794e10978c615
Jabi <michal.jabi@gmail.com>
2025-07-10 12:32:02 +0200

    task(6): make it work

--- JSNO-zadania/6-use-npm-library-to-color/party-guests/src/main.js
+++ JSNO-zadania/6-use-npm-library-to-color/party-guests/src/main.js
@@ -1,12 +1,24 @@
 import { guestsService } from './guests/guests.service.js';
+import chalk from 'chalk'
 
 try {
     await guestsService.initData();
 
-    guestsService.addGuest('Jan', 'Kowalski');
-    console.table(guestsService.getAll())
-
-    await guestsService.saveData();
+    guestsService.getAll().forEach((guest, idx) => {
+        switch(guest.status) {
+            case "invited":
+                console.log(idx + 1, chalk.blue(`${guest.name} ${guest.lastName}`));
+                break;
+            case "confirmed":
+                console.log(idx + 1,chalk.green(`${guest.name} ${guest.lastName}`));
+                break;
+            case "declined":
+                console.log(idx + 1, chalk.red(`${guest.name} ${guest.lastName}`));
+                break;
+            default:
+                console.log(idx + 1, `${guest.name} ${guest.lastName}`);
+        }
+    })
 } catch (e) {
     console.error(e);
 }
