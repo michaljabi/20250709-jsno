735a55ed47d00cab758a1f9a0180995b74d574d4
Jabi <michal.jabi@gmail.com>
2025-07-10 09:12:51 +0200

    task(05 teoria): make it work (.then() / .catch())

--- JSNO-teoria/00-test-your-might/05-promise-intro.js
+++ JSNO-teoria/00-test-your-might/05-promise-intro.js
@@ -1,12 +1,36 @@
 
 console.log('Hello stranger... shall we start?')
 
+// number[]
+// Array<number>
+const numbers = [1, 2, 3, 4, '', true]
+
 function giveMeTheNumber() {
     return Promise.resolve(300)
 }
 
+await sth();
 /**
  * 1
  *
  * Poniżej odbierz liczbę 300 i pokaż ją na ekranie:
- * */
+ * */ 
+
+giveMeTheNumber().then(num => {
+    console.log(num);
+})
+
+
+function giveMeTheNumber2() {
+    return Promise.reject(456)
+}
+
+/**
+ * 2
+ *
+ * Poniżej odbierz liczbę 456 i pokaż ją na ekranie:
+ * */ 
+
+giveMeTheNumber2().catch(num => {
+    console.log(num);
+})
