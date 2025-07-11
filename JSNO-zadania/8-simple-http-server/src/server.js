673e2f68b7ed5ebaf6ffd3398e1a9e1e22dc9d99
Jabi <michal.jabi@gmail.com>
2025-07-11 09:51:25 +0200

    task(8)[part-4]: import `.json` to .js

--- JSNO-zadania/8-simple-http-server/src/server.js
+++ JSNO-zadania/8-simple-http-server/src/server.js
@@ -1,6 +1,7 @@
 import {createServer} from 'node:http'
 import process from 'node:process';
-// side-quest: obsługa błędów
+import data from './data.json' with { type: 'json' }
+
 
 
 const { PORT } = process.env;
@@ -8,11 +9,20 @@ const { PORT } = process.env;
 const server = createServer((req, res) => {
 
     // throw new Error('Oh No!')
-    res.statusCode = 404
+   
     res.setHeader('Content-Type', 'application/json')
-    res.write(`port: ${PORT}`)
+
+    switch(req.url) {
+        case "/data":
+            res.statusCode = 200
+            res.write(JSON.stringify(data))
+            break;
+        default:
+            res.statusCode = 404
+            res.write(JSON.stringify({ "error": `404 - nie znam ścieżki ${req.url}`  }))
+    }
+
     res.end();
-    // process.exit(0);
 });
 
 server.listen(PORT)
@@ -21,7 +31,7 @@ server.on('listening', () => {
     console.log(`jestem... na procie: ${PORT}`)
 })
 
-
+// side-quest: obsługa błędów
 process.on('uncaughtException', (e) => {
     console.error(e)
     console.log('----- złapane !')
