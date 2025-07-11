38b8ef7bb95aead855e9003983d8d18921f75fef
Jabi <michal.jabi@gmail.com>
2025-07-11 10:22:16 +0200

    task(8.1): make it work. Prod start with pm2

--- JSNO-zadania/8-simple-http-server/src/server.js
+++ JSNO-zadania/8-simple-http-server/src/server.js
@@ -5,7 +5,7 @@ import { jsonFileService } from './shared/json-file.service.js';
 // import data from './data.json' with { type: 'json' }
 
 
-const { PORT } = process.env;
+const { PORT = 4000 } = process.env;
 
 const server = createServer(async (req, res) => {
 
@@ -18,10 +18,12 @@ const server = createServer(async (req, res) => {
             res.statusCode = 200
             const data = await jsonFileService.getDataFrom(join(import.meta.dirname, './data.json'));
             res.write(JSON.stringify(data))
+            console.log('Data requested', req.headers)
             break;
         default:
             res.statusCode = 404
             res.write(JSON.stringify({ "error": `404 - nie znam ścieżki ${req.url}`  }))
+            console.warn(`404 not found ${req.url}`, req.headers)
     }
 
     res.end();
